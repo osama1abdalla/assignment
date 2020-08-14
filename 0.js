8fbc78cde0895c9006abd281a73d276b355ace2d
osama abdalla <osama222@outlook.com>
2020-08-14 17:29:23 +0200

    edit 1

---
+++ 0.js
@@ -0,0 +1,16 @@
+// var x = 0
+// while (x <= 100)
+// {
+//     console.log(x)
+//     x++
+// }
+
+// for (i=0 ;i <= 100 ;i++)
+// {
+//     console.log(i)
+// }
+
+var x =['osama',1,'abdalla']
+console.log(x)
+console.log('Name:')
+console.log(x[0])
---
+++ ch1.js
@@ -0,0 +1,17 @@
+// challenge 1
+// Convert Minutes into Seconds
+
+m = 5
+console.log(s = m * 60)
+//END
+
+//challenge 2
+// Area of a Triangle
+
+var base = 5
+var h = 6
+console.log((base*h)/2)
+//END
+
+// challenge 3
+// Find the Smallest and Biggest Numbers
